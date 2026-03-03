cc42f1cdf4f3f6de4a9eefa619f9a4c5b3b41b8d
Ishak_Karatas <ishakelz23@gmail.com>
2026-03-03 10:33:46 +0300

    chore: initial project setup with Vite + React

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB - 1</h2>
+      <p>Ad Soyad: Ishak Karataş</p>
+      <p>Ogrenci No: 235541073</p>
+    </div>
+  );
+}
+
+export default App;
